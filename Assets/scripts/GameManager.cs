0713b4e2f4629f5acb8450286ee027aee16c46c0
Karen <photo.karen@gmail.com>
2020-02-21 22:29:16 -0800

    test to see if this works

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -7,17 +7,28 @@ public class GameManager : MonoBehaviour
 {
     public Text spookyValueText;
     public int SpookyValue = 0;
+    public Text timerValueText;
+    public float timerValue;
+    public Text bestTimeTextValue;
+    public float bestTimeValue;
+
+    private bool isGameRunning = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGameRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGameRunning)
+        {
+            timerValue += Time.deltaTime;
+            timerValueText.text = timerValue.ToString();
+        }
     }
 
     public void AddToSpookyValue()
