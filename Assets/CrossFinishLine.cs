2ad8039a47dba716258d948ea8e66cdd4ceb2c86
Karen <photo.karen@gmail.com>
2020-02-21 23:38:42 -0800

    to turn in
    Tracking best time, Spooky value pick ups (if more than 1 spooky is printed in the console. The enemy plays a particle effect (gets scared) and is subjected to physics.

--- Assets/CrossFinishLine.cs
+++ Assets/CrossFinishLine.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CrossFinishLine : MonoBehaviour
 {
+
+    public GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class CrossFinishLine : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gm.PlayerCrossedFinishLine();
     }
 }
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -11,14 +11,24 @@ public class GameManager : MonoBehaviour
     public float timerValue;
     public Text bestTimeTextValue;
     public float bestTimeValue;
+    public Button restartButton;
+
+    public Transform player;
+    public Transform resetPlayerPosition;
+
+    public ParticleSystem winParicles;
+    public ParticleSystem playerSpookyParticles;
 
     private bool isGameRunning = false;
+    private bool firsTimePlaying = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         isGameRunning = true;
+        firsTimePlaying = true;
+        restartButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -29,11 +39,50 @@ public class GameManager : MonoBehaviour
             timerValue += Time.deltaTime;
             timerValueText.text = timerValue.ToString();
         }
+
+        if (SpookyValue > 2)
+        {
+            Debug.Log("spooky");
+            playerSpookyParticles.Play();
+        }
     }
 
     public void AddToSpookyValue()
     {
-        SpookyValue+= 1;
+        SpookyValue++;
         spookyValueText.text = SpookyValue.ToString();
+    }
+
+    public void PlayerCrossedFinishLine()
+    {
+        isGameRunning = false;
+        winParicles.Play();
+        if (firsTimePlaying == false)
+        {
+            if (timerValue < bestTimeValue)
+            {
+                bestTimeValue = timerValue;
+                bestTimeTextValue.text = bestTimeValue.ToString();
+            }
+        } else
+        {
+            bestTimeValue = timerValue;
+            bestTimeTextValue.text = bestTimeValue.ToString();
+            
+        }
+        firsTimePlaying = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void resetGame()
+    {
+        winParicles.Stop();
+        playerSpookyParticles.Stop();
+        restartButton.gameObject.SetActive(false);
+        player.position = resetPlayerPosition.position;
+        timerValue = 0;
+        SpookyValue = 0;
+        isGameRunning = true;
+
     }
 }
